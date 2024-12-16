1c0b9f1efd3fe133b0bcb0d26f964b5f7fb4248e
Saisana299 <yt0521kariaka@gmail.com>
2024-12-16 15:24:31 +0900

    [Edit]: changed color theme.

--- lib/appview_binder.dart
+++ lib/appview_binder.dart
@@ -11,6 +11,30 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: _appRouter.config(),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 245, 245, 245),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          shape: Border(
+            bottom: BorderSide(
+              color: Color.fromARGB(255, 222, 222, 222),
+              width: 1.0,
+            ),
+          ),
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.white,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -32,12 +32,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: App(),
-    );
+    return App();
   }
 }
