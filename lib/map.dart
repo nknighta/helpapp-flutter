4d6b38784455f2693e5e43bad2ed212ab2fc2146
Saisana299 <yt0521kariaka@gmail.com>
2025-01-17 20:47:36 +0900

    [Edit]: mapbox annotation.

--- lib/map.dart
+++ lib/map.dart
@@ -1,6 +1,7 @@
 // map.dart
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:help_app/env/env.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
@@ -17,6 +18,34 @@ class _MapPageState extends State<MapPage> {
 
   _onMapCreated(MapboxMap mapboxmap) {
     mapboxMap = mapboxmap;
+    addPinToMap();
+  }
+
+  Future<void> addPinToMap() async {
+    final PointAnnotationManager? annotationManager = await mapboxMap?.annotations.createPointAnnotationManager();
+    final bytes = await rootBundle.load('assets/images/annotation_pin.png');
+    final list = bytes.buffer.asUint8List();
+
+    final positions = [
+      Position(Env.defaultMapLng, Env.defaultMapLat),
+    ];
+
+    var options = <PointAnnotationOptions>[];
+    for (var i = 0; i < positions.length; i++) {
+      options.add(
+        PointAnnotationOptions(
+          geometry: Point(
+            coordinates: positions[i],
+          ),
+          textField: "HELPER POINT",
+          textOffset: [0.0, -2.0],
+          textColor: Colors.red.value,
+          symbolSortKey: 10,
+          image: list
+        )
+      );
+    }
+    annotationManager?.createMulti(options);
   }
 
   @override
